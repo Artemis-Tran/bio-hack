7dd3c2ac4be68271b026d5dc98109a84d30c7896
Artemis Tran <artemistranhb@gmail.com>
2023-12-31 08:02:03 -0800

    Cleaned up main.py more

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, r
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
 import matplotlib.pyplot as plt
+import joblib
 
 # defined macros
 K_VALUE = 2
@@ -18,6 +19,7 @@ TEST_FASTA_FILE_PATH = os.path.abspath("test.fasta")
 
 ### PRE-PROCESSING DATA
 
+# function to count and calculate kmers for a given sequence
 def cal_kmers(s, k):
     count_kmers = {}
     for i in range(len(s) - k + 1):
@@ -28,61 +30,82 @@ def cal_kmers(s, k):
             count_kmers[kmer] += 1
     return count_kmers
 
-# Setup an empty list
-kmers_list = []  
-for record in SeqIO.parse(ACCESSIBLE_FASTA_FILE_PATH, "fasta"):
-    kmers_list.append(cal_kmers(record.seq, K_VALUE))
+### TRAINING MODEL
+def train_model():
 
-MAX_LENGTH = 999999
-for record in SeqIO.parse(NOTACCESSIBLE_FASTA_FILE_PATH, "fasta"):
-    kmers_list.append(cal_kmers(record.seq, K_VALUE))
-    if len(kmers_list) > MAX_LENGTH:
-        break
+    #filling list with kmer calculations for each sequence
+    kmers_list = []  
+    for record in SeqIO.parse(ACCESSIBLE_FASTA_FILE_PATH, "fasta"):
+        kmers_list.append(cal_kmers(record.seq, K_VALUE))
+    MAX_LENGTH = 999999
+    for record in SeqIO.parse(NOTACCESSIBLE_FASTA_FILE_PATH, "fasta"):
+        kmers_list.append(cal_kmers(record.seq, K_VALUE))
+        if len(kmers_list) > MAX_LENGTH:
+            break
 
-test_kmers_list = []
-test_sequence_ids = []
-for record in SeqIO.parse(TEST_FASTA_FILE_PATH, "fasta"):
-    test_kmers_list.append(cal_kmers(record.seq, K_VALUE))
-    test_sequence_ids.append(record.id)
+    # Creating 2d matrix with kmer counts
+    matrix = pd.DataFrame(kmers_list)
+    matrix.fillna(0, inplace=True)
 
-matrix = pd.DataFrame(kmers_list)
-matrix.fillna(0, inplace=True)
+    # Creating approriate labels, 0 for accessible and 1 for not accessible
+    accessible_labels = [0] * (47239)
+    non_accessible_labels = [1] * (len(kmers_list) - 47239)
+    labels = accessible_labels + non_accessible_labels
 
-test_matrix = pd.DataFrame(test_kmers_list)
-test_matrix.fillna(0, inplace=True)
+    # Splitting the training data to training data and testing data to test accuracy
+    X_train, X_test, y_train, y_test = train_test_split(matrix, labels, test_size=0.2)
 
-accessible_labels = [0] * (47239)
-non_accessible_labels = [1] * (len(kmers_list) - 47239)
-labels = accessible_labels + non_accessible_labels
+    # Initializing and fitting the random forest to the training data
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf.fit(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(matrix, labels, test_size=0.2)
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    # Making predictions on the testing set
+    y_pred = clf.predict(X_test)
 
-clf.fit(X_train, y_train)
+    # Making confusion matrix png
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.savefig('confusion_matrix.png')
 
-# Make predictions on the testing set
-y_pred = clf.predict(X_test)
+    # save the model to a file
+    model_filename = "random_forest_model.joblib"
+    joblib.dump(clf, model_filename)
+    print(f"Trained model saved to {model_filename}")
 
-print("Found %i entries" % len(kmers_list))
-print(matrix)
+    # Evaluate the model
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy}")
+    return clf
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+### PREDICTING RESULTS
+def predict_test(clf):
 
-cm = confusion_matrix(y_test, y_pred)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot()
-plt.savefig('confusion_matrix.png')
+    #filling list with kmer calculations for each sequence and creating corresponding list of ids
+    test_kmers_list = []
+    test_sequence_ids = []
+    for record in SeqIO.parse(TEST_FASTA_FILE_PATH, "fasta"):
+        test_kmers_list.append(cal_kmers(record.seq, K_VALUE))
+        test_sequence_ids.append(record.id)
 
-test_predictions = clf.predict(test_matrix)
+    # Create 2d matrix 
+    test_matrix = pd.DataFrame(test_kmers_list)
+    test_matrix.fillna(0, inplace=True)
 
-output_file = "top_accessible_sites.txt"
-with open(output_file, 'w') as file:
-    count = 0
-    for sequence_id, prediction in zip(test_sequence_ids, test_predictions):
-        if prediction == 0:  # predicted as accessible
-            file.write(f"{sequence_id}\n")
-            count += 1
-            if count >= 10000:
-                break
+    # make predictions based on training
+    test_predictions = clf.predict(test_matrix)
+
+    # making file with our predicted accessible sites
+    output_file = "predictions.csv"
+    with open(output_file, 'w') as file:
+        count = 0
+        for sequence_id, prediction in zip(test_sequence_ids, test_predictions):
+            if prediction == 0:  # predicted as accessible
+                file.write(f"{sequence_id}\n")
+                count += 1
+                if count >= 10000:
+                    break
+
+if __name__ == "__main__":
+    clf = train_model()
+    predict_test(clf)
