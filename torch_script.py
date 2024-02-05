c78adb6e61875175d41234e4068ad97268561b6d
Artemis Tran <artemistranhb@gmail.com>
2024-02-05 14:57:18 -0800

    Finalized layers for model and tuned hyperparameters

--- torch_script.py
+++ torch_script.py
@@ -7,28 +7,34 @@ import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 from torch import nn
+import time
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-K_VALUE = 2
-MAX_LENGTH = 299999
 ACCESSIBLE_FASTA_FILE_PATH = os.path.abspath("accessible.fasta")
 NOTACCESSIBLE_FASTA_FILE_PATH = os.path.abspath("notaccessible.fasta")
 TEST_FASTA_FILE_PATH = os.path.abspath("test.fasta")
+
+# Hyperparameters
+K_VALUE = 2
+MAX_LENGTH = 299999
+HIDDEN_LAYER = 64
+EPOCHS = 1000
+LEARNING_RATE = 0.01
+DROPOUT = 0.5
+
 
 # Class Model
 class sequence_model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer_1 = nn.Linear(in_features=16, out_features=128)
-        self.layer_2 = nn.Linear(in_features=128, out_features=128)
-        self.layer_3 = nn.Linear(in_features=128, out_features=1)
+        self.layer_1 = nn.Linear(in_features=16, out_features=HIDDEN_LAYER)
+        self.layer_2 = nn.Linear(in_features=HIDDEN_LAYER, out_features=HIDDEN_LAYER)
+        self.layer_3 = nn.Linear(in_features=HIDDEN_LAYER, out_features=1)
+        
         self.relu = nn.ReLU()
-
     def forward(self,x):
-        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
-    
-
-### PRE-PROCESSING DATA
+        z = self.layer_2(self.relu(self.layer_1(x)))
+        return self.layer_3(z)
 
 # function to count and calculate kmers for a given sequence
 def cal_kmers(s, k):
@@ -46,67 +52,19 @@ def accuracy_fn(y_true, y_pred):
     acc = (correct/len(y_pred)) * 100
     return acc
 
-### TRAINING MODEL
-def prepare_data():
-    print("Preparing Data...")
-    #filling list with kmer calculations for each sequence
-    kmers_list = []  
-    for record in SeqIO.parse(ACCESSIBLE_FASTA_FILE_PATH, "fasta"):
-        kmers_list.append(cal_kmers(record.seq, K_VALUE))
-    accessible_labels = [0] * len(kmers_list)
-
-    
-    for record in SeqIO.parse(NOTACCESSIBLE_FASTA_FILE_PATH, "fasta"):
-        kmers_list.append(cal_kmers(record.seq, K_VALUE))
-        if len(kmers_list) > MAX_LENGTH:
-            break
-    non_accessible_labels = [1] *(len(kmers_list) - len(accessible_labels))
-
-    all_keys = sorted(set().union(*[d.keys() for d in kmers_list]))
-    ordered_kmers_list = [{key: d.get(key, 0) for key in all_keys} for d in kmers_list]
-    y = np.array(accessible_labels + non_accessible_labels)
-    kmers_list_numeric = [list(count_kmers.values()) for count_kmers in ordered_kmers_list]
-
-    X = np.array(kmers_list_numeric)
-    matrix = pd.DataFrame(X)
-    print(matrix)
-    X = torch.from_numpy(X).type(torch.float)
-    y = torch.from_numpy(y).type(torch.float)
-    print(X)
-    print(y[:10])
-    print(X.shape)
-    print(y.shape)
-    
-    # Splitting data for training and testing
-    X_train, X_test, y_train, y_test = train_test_split(X ,y , test_size=0.2, random_state=42)
-    
-    # Model to train
-    model_0 = sequence_model().to(DEVICE)
-    
-
-    # Loss function and optimizer
-    loss_fn = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01)
-
-    # logits -> prediction probablities -> prediction lablels
-    # y_pred_labels = torch.round(torch.sigmoid(model_0(X_test.to(DEVICE))))
-
+def train_model(X_train, y_train, X_test, y_test, model, epochs):
     torch.manual_seed(42)
     torch.cuda.manual_seed(42)
 
-    # Number of iterations 
-    epochs = 1000
-
-    # Putting data to right device
-    X_train, y_train = X_train.to(DEVICE), y_train.to(DEVICE) 
-    X_test, y_test = X_test.to(DEVICE), y_test.to(DEVICE) 
+    loss_fn = nn.BCEWithLogitsLoss() 
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=LEARNING_RATE, weight_decay=0.001)
 
     for epoch in range(epochs):
         ### Training
-        model_0.train()
+        model.train()
 
         # Forward Pass
-        y_logits = model_0(X_train).squeeze()
+        y_logits = model(X_train).squeeze()
         y_pred = torch.round(torch.sigmoid(y_logits))
 
         # Calculate Loss
@@ -122,10 +80,10 @@ def prepare_data():
         # Step
         optimizer.step()
 
-        model_0.eval()
+        model.eval()
         with torch.inference_mode():
             # Forward Pass
-            test_logits = model_0(X_test).squeeze()
+            test_logits = model(X_test).squeeze()
             test_pred = torch.round(torch.sigmoid(test_logits))
 
             # Calculate Loss
@@ -134,10 +92,58 @@ def prepare_data():
 
             if epoch % 10 == 0:
                 print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+    return model
 
-    return model_0
+def prepare_data():
+    print("Processing Data...")
+    #filling list with kmer calculations for each sequence
+    kmers_list = []  
+    for record in SeqIO.parse(ACCESSIBLE_FASTA_FILE_PATH, "fasta"):
+        kmers_list.append(cal_kmers(record.seq, K_VALUE))
+    accessible_labels = [1] * len(kmers_list)
+
+    
+    for record in SeqIO.parse(NOTACCESSIBLE_FASTA_FILE_PATH, "fasta"):
+        kmers_list.append(cal_kmers(record.seq, K_VALUE))
+        if len(kmers_list) > MAX_LENGTH:
+            break
+    non_accessible_labels = [0] *(len(kmers_list) - len(accessible_labels))
+
+    all_keys = sorted(set().union(*[d.keys() for d in kmers_list]))
+    ordered_kmers_list = [{key: d.get(key, 0) for key in all_keys} for d in kmers_list]
+    kmers_list_numeric = [list(count_kmers.values()) for count_kmers in ordered_kmers_list]
+
+    y = np.array(accessible_labels + non_accessible_labels)
+    X = np.array(kmers_list_numeric)
+    matrix = pd.DataFrame(X)
+    print(matrix)
+    X = torch.from_numpy(X).type(torch.float)
+    y = torch.from_numpy(y).type(torch.float)
+    print(X.shape)
+    print(y.shape)
+
+    # Splitting data for training and testing
+    X_train, X_test, y_train, y_test = train_test_split(X ,y , test_size=0.2, random_state=42)
+    X_train, y_train = X_train.to(DEVICE), y_train.to(DEVICE) 
+    X_test, y_test = X_test.to(DEVICE), y_test.to(DEVICE) 
+
+    # Model to train
+    model_0 = sequence_model().to(DEVICE)
+    
+    print("Creating weights...")
+    num_accessible = (y_train == 1).sum().item()
+    num_not_accessible = (y_train == 0).sum().item()
+    weight = torch.tensor([num_not_accessible / num_accessible]).to(DEVICE)
+
+    # Loss function and optimizer
+    loss_fn = nn.BCEWithLogitsLoss() 
+    optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01, weight_decay=0.001)
+
+    nn_model = train_model(X_train, y_train, X_test, y_test, model_0, EPOCHS)
+    return nn_model
 
 def predict_test(model):
+    print("Predicting test data...")
     test_kmers_list = []
     test_sequence_ids = []
     for record in SeqIO.parse(TEST_FASTA_FILE_PATH, "fasta"):
@@ -156,23 +162,24 @@ def predict_test(model):
     predictions = torch.Tensor.cpu(predictions)
     predictions = torch.Tensor.numpy(predictions)
 
+    print("Creating predictions.csv...")
     output_file = "predictions.csv"
     with open(output_file, 'w') as file:
         count = 0
         for sequence_id, prediction in zip(test_sequence_ids, predictions):
-            if prediction[0] == 0:  # predicted as accessible
+            if prediction[0] == 1:  # predicted as accessible
                 file.write(f"{sequence_id}\n")
                 count += 1
                 if count >= 10000:
                     break
     return predictions
 
-    # Evaluation metric
-    
-
 if __name__ == "__main__":
+    time_start = time.time()
     model = prepare_data()
     print(predict_test(model))
+    time_elapsed = (time.time() - time_start) / 60
+    print(f"Program ran for {time_elapsed} minutes")
 
 
 
